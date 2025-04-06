d3ca8fee30e81ac4be1b4d5bce5c1cf6f8ac9a93
manelfatnassi <manel.fatnassi@esprit.tn>
2025-04-06 00:46:00 +0000

    eureka

--- backend/src/main/java/com/library/loanservice/LoanServiceApplication.java
+++ backend/src/main/java/com/library/loanservice/LoanServiceApplication.java
@@ -3,7 +3,9 @@ package com.library.loanservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class LoanServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoanServiceApplication.class, args);
